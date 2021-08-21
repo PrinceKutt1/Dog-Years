1d929378be538e3cccc7cd32d5a739c146e6bfec
Kutorglo Prince Wells <87288156+PrinceKutt1@users.noreply.github.com>
2021-08-22 00:48:02 +1100

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+let myAge = 31;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+//later years equivalent in dog years
+laterYears *= 4;
+
+//computing equivalent of total age to dog years
+let myAgeInDogYears = earlyYears + laterYears
+
+let myName = "Kutorglo Prince Wells".toLowerCase();
+console.log(`My name is ${myName }. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
